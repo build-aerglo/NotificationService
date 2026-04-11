29d1c1e9175247f070eff4fa382c8bd5ff30dfb8
Oseahumen Agboifoh <oseahumenagboifoh@gmail.com>
2026-04-11 21:21:37 +0100

    added missing files and minor fixes after vulnerabilty testing

---
+++ src/NotificationService.Application/Constants/NotificationChannels.cs
@@ -0,0 +1,8 @@
+﻿namespace NotificationService.Application.Constants;
+
+public static class NotificationChannels
+{
+    public const string Sms = "sms";
+    public const string Email = "email";
+    public const string InApp = "in-app";
+}
---
+++ src/NotificationService.Application/Constants/NotificationTemplates.cs
@@ -0,0 +1,8 @@
+﻿namespace NotificationService.Application.Constants;
+
+public static class NotificationTemplates
+{
+    public const string Otp = "otp";
+    public const string PasswordReset = "password-reset";
+    public const string BusinessVerification = "business-verification";
+}
